547e5f3d43b04bda3b06e10b360d9c61fe4a340b
Silent-Gamez <brenensines@gmail.com>
2023-07-11 09:25:52 -0400

    Added a little bit of content

--- script2.js
+++ script2.js
@@ -11,10 +11,12 @@ opened = 0
 function open_menu() {
   if (opened === 0) {
     document.querySelector("#color_menu").style.display = "flex";
+    document.querySelector("#color_menu").style.position = "fixed";
     opened = 1;
   }
   else {
     document.querySelector("#color_menu").style.display = "none";
+    document.querySelector("#color_menu").style.position = "inline";
     opened = 0;
   }
 };
